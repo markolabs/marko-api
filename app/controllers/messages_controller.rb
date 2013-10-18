d0c30f054f4431ba4ae2e80250d507e322ff45dd
Spencer Schoeben <spencer@netspencer.com>
2013-10-18 11:34:33 -0400

    changing default radius to 0.1

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < RocketPants::Base
   def index
     unless (params[:latitude].nil? && params[:longitude].nil?)
-      params[:radius] ||= 1
+      params[:radius] ||= 0.1
       messages = Message.order("created_at DESC").limit(20).near([params[:latitude], params[:longitude]], params[:radius])
     else
       messages = Message.order("created_at DESC").limit(20)
