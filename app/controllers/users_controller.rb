95685bf7779974c1139c5387eb1560b95ea70a6e
Spencer Schoeben <spencer@netspencer.com>
2013-11-02 13:45:36 -0400

    accomodating multiple user_id keys for me value hash replacement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApiController
   before_filter :current_user
-  
+
   def show
     user = User.find(params[:id])
     expose user
@@ -18,5 +18,11 @@ class UsersController < ApiController
       require_login
       params[:id] = @current_user.id 
     end
+
+    if params[:user_id] == "me"
+      require_login
+      params[:user_id] = @current_user.id
+    end
+
   end
 end
