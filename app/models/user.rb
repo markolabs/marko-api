a5f513e25e8ec249dfa67909988c9da7f0380ca1
Spencer Schoeben <spencer@netspencer.com>
2013-11-08 00:31:38 -0500

    removing devise ewww

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,11 +26,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :omniauthable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # devise :database_authenticatable, :registerable, :omniauthable,
+  #        :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # # Setup accessible (or protected) attributes for your model
+  # attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :fb_user_id, :username
 
   validates :username, presence: true, uniqueness: true
