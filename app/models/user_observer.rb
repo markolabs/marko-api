34bbca1a62432b22d5050de20dbc3d1d299ba91e
Spencer Schoeben <spencer@netspencer.com>
2014-01-07 21:52:24 -0800

    adding user to people mixpanel creation

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -5,5 +5,23 @@ class UserObserver < ActiveRecord::Observer
     mixpanel.track "User Created", {
       distinct_id: user.id
     }
+
+    mixpanel.track.people user.id, {
+      "$first_name": user.first_name,
+      "$last_name": user.first_name,
+      "Signup": user.created_at,
+      "Username": user.username,
+      "Facebook ID": user.fb_user_id
+    }
+  end
+
+  def after_save(user)
+    mixpanel.track.people user.id, {
+      "$first_name": user.first_name,
+      "$last_name": user.first_name,
+      "Signup": user.created_at,
+      "Username": user.username,
+      "Facebook ID": user.fb_user_id
+    }
   end
 end
