7ee9defc19f232c681ab130c365962cfe9ada1f0
Spencer Schoeben <spencer@netspencer.com>
2014-01-07 21:54:55 -0800

    because i sux at syntax

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -7,21 +7,21 @@ class UserObserver < ActiveRecord::Observer
     }
 
     mixpanel.track.people user.id, {
-      "$first_name": user.first_name,
-      "$last_name": user.first_name,
-      "Signup": user.created_at,
-      "Username": user.username,
-      "Facebook ID": user.fb_user_id
+      "$first_name" => user.first_name,
+      "$last_name" => user.first_name,
+      "Signup" => user.created_at,
+      "Username" => user.username,
+      "Facebook ID" => user.fb_user_id
     }
   end
 
   def after_save(user)
     mixpanel.track.people user.id, {
-      "$first_name": user.first_name,
-      "$last_name": user.first_name,
-      "Signup": user.created_at,
-      "Username": user.username,
-      "Facebook ID": user.fb_user_id
+      "$first_name" => user.first_name,
+      "$last_name" => user.first_name,
+      "Signup" => user.created_at,
+      "Username" => user.username,
+      "Facebook ID" => user.fb_user_id
     }
   end
 end
